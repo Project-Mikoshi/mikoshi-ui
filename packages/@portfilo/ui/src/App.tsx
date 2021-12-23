9e53f7d0a9fefd971e134b3a569d6ef5b587d5f7
Anthony <anthonyzhu1114@gmail.com>
2021-12-22 21:25:51 -0500

    - remove ui

--- packages/@portfilo/ui/src/App.tsx
+++ packages/@portfilo/ui/src/App.tsx
@@ -1,10 +1,31 @@
-import './App.css'
+import React from 'react'
+import './App.scss'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import NavBar, { RouteData } from 'components/NavBar/component'
+import ProjectCardDemo from 'demo/ProjectCard/page'
 
-function App() {
+const demoRoutes: Array<RouteData> = [
+  {label: 'Home', route: '/'},
+  {label: 'Project Card Demo', route: 'project-card-demo'}
+]
+
+export default function App () {
   return (
-    <div className="App">
+    <div className="app">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      <Router>
+        <NavBar routes={demoRoutes} />
+
+        <Switch>
+          <Route path='/project-card-demo'>
+            <ProjectCardDemo />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
-
-export default App
