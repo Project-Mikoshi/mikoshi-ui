f1b7e37f500c6bbfc2e4f13e3a13e151a9c1ec0d
Anthony Y. Zhu <41710998+anthony-y-zhu14@users.noreply.github.com>
2022-08-13 15:21:46 -0400

    - update

--- packages/@mikoshi/core-components/vite.config.ts
+++ packages/@mikoshi/core-components/vite.config.ts
@@ -1,9 +1,13 @@
+import { fileURLToPath } from 'url'
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import typescript from '@rollup/plugin-typescript'
 import tsTransformPaths from '@zerollup/ts-transform-paths'
+
+// @ts-expect-error
+const __dirname = typeof __dirname !== 'undefined'? __dirname : path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig(() => ({
   build: {
