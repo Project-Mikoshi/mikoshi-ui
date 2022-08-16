b3862afe068bb2e7255ae979ffc1cee0f87a7c06
Anthony Y. Zhu <41710998+anthony-y-zhu14@users.noreply.github.com>
2022-08-16 10:34:34 -0400

    - try to fix storybook

--- packages/@mikoshi/core-components/test/components/tabs.test.tsx
+++ packages/@mikoshi/core-components/test/components/tabs.test.tsx
@@ -4,7 +4,7 @@ import { describe, it, expect } from 'vitest'
 import { Tabs, Tab } from '@mikoshi/core-components'
 import '@testing-library/jest-dom'
 
-describe('components/application/app-bar', () => {
+describe('components/application/tabs', () => {
   it('should render', () => {
     const TABS = [
       { value: 1, label: 'Tab 1' },
