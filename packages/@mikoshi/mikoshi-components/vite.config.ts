5f5ebd08e4debe2ac33b2312eaaa1cdde540b22d
anthony-y-zhu14 <41710998+anthony-y-zhu14@users.noreply.github.com>
2023-07-03 11:53:28 -0400

    - upgrade

--- packages/@mikoshi/mikoshi-components/vite.config.ts
+++ packages/@mikoshi/mikoshi-components/vite.config.ts
@@ -7,43 +7,45 @@ import dts from 'vite-plugin-dts'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
 // @ts-expect-error
-const __dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url))
+export default defineConfig(() => {
+  // eslint-disable-next-line no-undef
+  const DIRECTORY_NAME = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url))
 
-// @ts-expect-error
-export default defineConfig(() => ({
-  build: {
-    lib: {
-      entry: {
-        core: path.resolve(__dirname, 'src/core.ts'),
-        specialized: path.resolve(__dirname, 'src/specialized.ts')
+  return {
+    build: {
+      lib: {
+        entry: {
+          core: path.resolve(DIRECTORY_NAME, 'src/core.ts'),
+          specialized: path.resolve(DIRECTORY_NAME, 'src/specialized.ts')
+        },
+        formats: ['es']
       },
-      formats: ['es']
+      rollupOptions: {
+        plugins: [
+          peerDepsExternal()
+        ]
+      }
     },
-    rollupOptions: {
-      plugins: [
-        peerDepsExternal()
-      ]
+    resolve: {
+      alias: {
+        '@/components/': path.resolve(DIRECTORY_NAME, 'src/components'),
+        '@/utils': path.resolve(DIRECTORY_NAME, 'src/utils'),
+        '@/constants': path.resolve(DIRECTORY_NAME, 'src/constants'),
+        '@/types': path.resolve(DIRECTORY_NAME, 'src/types'),
+        '@/svgs': path.resolve(DIRECTORY_NAME, 'src/svgs')
+      }
+    },
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      splitVendorChunkPlugin(),
+      dts({
+        entryRoot: 'src'
+      })
+    ],
+    test: {
+      environment: 'jsdom',
+      globals: true
     }
-  },
-  resolve: {
-    alias: {
-      '@/components/': path.resolve(__dirname, 'src/components'),
-      '@/utils': path.resolve(__dirname, 'src/utils'),
-      '@/constants': path.resolve(__dirname, 'src/constants'),
-      '@/types': path.resolve(__dirname, 'src/types'),
-      '@/svgs': path.resolve(__dirname, 'src/svgs')
-    }
-  },
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    splitVendorChunkPlugin(),
-    dts({
-      entryRoot: 'src'
-    })
-  ],
-  test: {
-    environment: 'jsdom',
-    globals: true
   }
-}))
+})
